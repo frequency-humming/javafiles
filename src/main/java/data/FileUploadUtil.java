1f187730272c9f6c4db55057c0c0c49de05f4c98
bruno signori <signoribruno@gmail.com>
2022-06-11 12:34:31 -0400

    added path

--- src/main/java/data/FileUploadUtil.java
+++ src/main/java/data/FileUploadUtil.java
@@ -12,9 +12,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 public class FileUploadUtil {
 	
-	public static String saveFile(String fileName, MultipartFile multipartFile)
-            throws IOException {
-        Path uploadPath = Paths.get("Files-Upload");
+	public static String saveFile(String fileName, MultipartFile multipartFile) throws IOException {
+        Path uploadPath = Paths.get("Files-Upload/pdf");
           
         if (!Files.exists(uploadPath)) {
             Files.createDirectories(uploadPath);
